4e7b3d9a1be0234d33a4f497e57b1e0358577d28
vp0010 <144505885+vp0010@users.noreply.github.com>
2024-06-22 08:13:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Here you can add your form submission logic
+    // For example, you can use Fetch API to send form data to a server
+    // You can also add form validation logic here
+    alert('Form submitted successfully!');
+  });
+  
